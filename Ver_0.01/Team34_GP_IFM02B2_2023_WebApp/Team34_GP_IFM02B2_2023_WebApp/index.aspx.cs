55155b8ae699932940e8f6798e10e3dd9f1f6cc7
Caitlyn Ross <caitlynross31@gmail.com>
2023-08-18 22:54:31 +0200

    Created Skeleton project and added basic database

---
+++ Ver_0.01/Team34_GP_IFM02B2_2023_WebApp/Team34_GP_IFM02B2_2023_WebApp/index.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//add service reference using
+using Team34_GP_IFM02B2_2023_WebApp.Team34_ServiceReference;
+
+namespace Team34_GP_IFM02B2_2023_WebApp
+{
+    public partial class index : System.Web.UI.Page
+    {
+        ServiceClient sc = new ServiceClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (sc.Login("fred", "fred123") == true)
+                testbox.Text = "Logged in";
+            else
+                testbox.Text = "Failed to Log in";
+        }
+    }
+}
