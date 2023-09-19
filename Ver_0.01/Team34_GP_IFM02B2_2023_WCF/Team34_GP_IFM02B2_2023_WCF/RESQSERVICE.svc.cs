a94340037637d49133bb0d53ec9aef4ce564adce
ChaRichardBruh <108257886+ChaRichardBruh@users.noreply.github.com>
2023-09-20 00:18:15 +0200

    Made methods fro returning a filtered product list, started commenting code

--- Ver_0.01/Team34_GP_IFM02B2_2023_WCF/Team34_GP_IFM02B2_2023_WCF/RESQSERVICE.svc.cs
+++ Ver_0.01/Team34_GP_IFM02B2_2023_WCF/Team34_GP_IFM02B2_2023_WCF/RESQSERVICE.svc.cs
@@ -13,96 +13,49 @@ namespace Team34_GP_IFM02B2_2023_WCF
         public class RESQSERVICE : IRESQSERVICE
         {
             ResQFoods_DCSDataContext db = new ResQFoods_DCSDataContext();
-            public string DeleteUser(String uEmail)
-            {
-                throw new NotImplementedException();
-            }
-
-            public string GetData(int value)
-            {
-                return string.Format("You entered: {0}", value);
-            }
 
             public List<UserTable> GetEmployeeRecords(String uEmail)
             {
                 throw new NotImplementedException();
             }
-
+            //This mehthod checks if a user exists in the usertables database
             private bool checkUserValid(String email)
             {
 
             bool exists = (from u in db.UserTables
                          where u.Email.Equals(email)
                          select u).Any();
+            //Return whether or not user exists (search done by email)
             return exists;
-
-
-        }
+            }
 
             //Look for user record type, return that type
             //Make 3 methods, one for each type of user
             public int loginUser(String user, String pass)
             {
-            /* var syslog = (from u in db.UserTables
-                           where u.Email.Equals(user) &&
-                           u.HashedPassword.Equals(pass)
-                           select u).FirstOrDefault();*/
-
-            /*var u = new UserTable
-            {
-                Email = 90;
-            };*/
-
-
-                /*bool valid = false;
-                String con = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\USER\\Source\\Repos\\ResQFoods\\Ver_0.01\\Team34_GP_IFM02B2_2023_WCF\\Team34_GP_IFM02B2_2023_WCF\\App_Data\\ResQFoods_DB.mdf; Integrated Security = True";
-                String query = "SELECT Count(Email) FROM UserTable WHERE Email = @userEmail AND HashedPassword = @userPass";
-                SqlConnection conn = new SqlConnection(con);
-                SqlCommand sCom = new SqlCommand(query, conn);
-                sCom.Parameters.AddWithValue("@userEmail", user);
-                sCom.Parameters.AddWithValue("@userPass", pass);
-                try
-                {
-                
-                    conn.Open();
-                    Object count = sCom.ExecuteScalar();
-                    Int32 var;
-                    if (Int32.TryParse(count.ToString(), out var))
-                    {
-                        if (var == 1)
-                        {
-                            valid = true;
-                        }
-                    }
-                }
-                catch (SqlException)
-                {
-                    Console.WriteLine("SQL ERROR");
-                }
-                finally
-                {
-                    conn.Close();
-                }
-                return valid;*/
-          
+            //If the users exists
             if(checkUserValid(user))
             {
+                //Search for user where email and hashed password match
                 var userT = (from u in db.UserTables
                               where u.Email.Equals(user) &&
                               u.HashedPassword.Equals(pass)
                               select u).FirstOrDefault(); 
-
+                //Return users type
                 int t = userT.UserType;
                 return t;
             }
+            //If user does not exist, return -1
             return -1;
             }
-            //Add an insert for the type of user registering (USERTYPE)
-
+            
+            //This method registers a user of into the parent table
             private bool regUser(String uEmail, String uPass, int type)
             {
+            //If the user does NOT exist
             if (!checkUserValid(uEmail))
             {
+                //Create new User, initialize with the passed values
                 UserTable a = new UserTable
                 {
                     Email = uEmail,
@@ -111,6 +64,7 @@ namespace Team34_GP_IFM02B2_2023_WCF
                     DateRegistered = System.DateTime.Today,
                     Enabled = true
                 };
+                //Insert user into database
                 try
                 {
                     db.UserTables.InsertOnSubmit(a);
@@ -122,39 +76,13 @@ namespace Team34_GP_IFM02B2_2023_WCF
                     e.GetBaseException();
                 }
             }
+            //If user exists, return false
             return false;
         }
+            //Method to register a sysAdmin
             public bool regAdmin(String uEmail, String uPass)
             {
-                /*if (checkValid(uEmail))
-                {
-                String con = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\USER\\Source\\Repos\\ResQFoods\\Ver_0.01\\Team34_GP_IFM02B2_2023_WCF\\Team34_GP_IFM02B2_2023_WCF\\App_Data\\ResQFoods_DB.mdf; Integrated Security = True";
-                String query = "INSERT INTO UserTable(UserType, HashedPassword, Email, DateRegistered, Enabled) VALUES(@param1, @param2, @param3, @param4, @param5 )";
-                SqlConnection conn = new SqlConnection(con);
-                    SqlCommand sCom = new SqlCommand(query, conn);
-                    sCom.Parameters.AddWithValue("@param1", uType);
-                    sCom.Parameters.AddWithValue("@param2", uPass);
-                    sCom.Parameters.AddWithValue("@param3", uEmail);
-                    sCom.Parameters.AddWithValue("@param4", uReg);
-                    sCom.Parameters.AddWithValue("@param5", true);
-                try
-                    {
-                        conn.Open();
-                        int recordsAffected = sCom.ExecuteNonQuery();
-                    }
-                    catch (SqlException)
-                    {
-                        // error here
-                    }
-                    finally
-                    {
-                        conn.Close();
-                    }
-
-                    return true;
-                }
-                return false;*/
-
+            //If the admin is added, return true
             if(regUser(uEmail, uPass, 0))
             {
                 return true;
@@ -164,15 +92,18 @@ namespace Team34_GP_IFM02B2_2023_WCF
 
             public bool regCust(String uEmail, String uPass, String fName, String lName, DateTime BDate, bool grant)
             {
+            //If the customers parent has been added to the database
             if (regUser(uEmail, uPass, 1))
             {
-               
+                //Get user that was just added to the database
                 var check = (from u in db.UserTables
                              where u.Email.Equals(uEmail)
                              select u).FirstOrDefault();
-
+                        
+                        //If the user has been successfully retrieved
                         if (check != null)
                         {
+                            //Create new customer, using the parent ID, and passed values
                             Customer c = new Customer
                             {
                                 UserId = check.UserId,
@@ -183,6 +114,7 @@ namespace Team34_GP_IFM02B2_2023_WCF
                             };
                         try
                         {
+                            //Submit new user
                             db.Customers.InsertOnSubmit(c);
                             db.SubmitChanges();
                              return true;
@@ -194,19 +126,24 @@ namespace Team34_GP_IFM02B2_2023_WCF
                         }
                         }
             }
+            //If parent not added, return false
             return false;
         }
 
+            //Method to register a store
             public bool regStore(String uEmail, String uPass, String comp, String name, String icoPath, String loc, String type)
-        {
+            {
+            //If the parent of the store has been added
             if (regUser(uEmail, uPass, 2))
             {
+                //Get the parent from the database
                 var check = (from u in db.UserTables
                              where u.Email.Equals(uEmail)
                              select u).FirstOrDefault();
 
                 if (check != null)
                 {
+                    //If the parent exists, create a store, using the parents ID
                     Store s = new Store
                     {
                         UserId = check.UserId,
@@ -218,6 +155,7 @@ namespace Team34_GP_IFM02B2_2023_WCF
                     };
                     try
                     {
+                        //Insert store into database
                         db.Stores.InsertOnSubmit(s);
                         db.SubmitChanges();
                         return true;
@@ -229,47 +167,22 @@ namespace Team34_GP_IFM02B2_2023_WCF
                     }
                 }
             }
+            //If parent was not added, return false
             return false;
 
-        }
+            }
             
-
+            //Method to return all products from database
             public List<Product> getAllProducts()
             {
-                /*String conn = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\USER\\Source\\Repos\\ResQFoods\\Ver_0.01\\Team34_GP_IFM02B2_2023_WCF\\Team34_GP_IFM02B2_2023_WCF\\App_Data\\ResQFoods_DB.mdf; Integrated Security = True";
-                String comm = "SELECT * FROM Products";
-                SqlConnection SqlConn = new SqlConnection(conn);
-                SqlCommand SqlComm = new SqlCommand(comm, SqlConn);
-                try
-                {
-                    SqlConn.Open();
-                    DbDataReader dbr = SqlComm.ExecuteReader();
-                    while(dbr.Read())
-                    {
-                        byte[] tempImg = null;
-                        ProductRecord temp = new ProductRecord();
-                        temp.prodID = dbr.GetInt32(0);
-                        temp.userID = dbr.GetInt32(1);
-                        temp.prodName = dbr.GetString(2);
-                        temp.prodDesc = dbr.GetString(3);
-                    
-
-                    }
-                }
-                catch(SqlException s)
-                {
-                    //Error handling logic here
-                }
-                finally
-                {
-                    SqlConn.Close();
-                }
-                throw new NotImplementedException();*/
+                //Create new lists, one for display, one for the data from the database
                 List<Product> prodRec = new List<Product>();
-                List<Product> prods = (from p in db.Products 
+                List<Product> prods = (from p in db.Products
+                                       where p.Enabled == true
                                         select p).ToList();
             if (prods.Any())
             {
+                //Create a new product, returning only what will be displayed, add this to the list that will be returned
                 foreach (Product p in prods)
                 {
                     Product pr = new Product
@@ -280,15 +193,78 @@ namespace Team34_GP_IFM02B2_2023_WCF
                         Picture = p.Picture,
                         Price = p.Price,
                         Description = p.Description,
-                        DateAdded = p.DateAdded,
+                        Enabled = p.Enabled,
                     };
                 prodRec.Add(pr);
 
                 }
-
             return prodRec;
             }
+            //Return null if no list found
             return null;
+            }
+
+            public List<Product> getFilteredList(String name, double P1, double P2, List<int> tags)
+            {
+            //Get list of all products
+            List<Product> tempList = getAllProducts();
+            //Filter if name not = null
+            if (name != "null")
+            {
+                for (int i = tempList.Count - 1; i > -1; i--)
+                {
+                    //If the name does not match the searched name, remove the item from the list
+                    if (tempList[i].Name != name)
+                    {
+                        tempList.RemoveAt(i);
+                    }
+                }
+            }
+
+            for (int i = tempList.Count - 1; i > -1; i--)
+            {
+                //Remove current value if it is greater or less than the bounds passed
+                if ((double)tempList[i].Price < P1 || (double)tempList[i].Price > P2)
+                {
+                    tempList.RemoveAt(i);
+                }
+            }
+            
+            //In the remaining list
+            for (int i = tempList.Count - 1; i > -1; i--)
+            {
+                //Create current product instance
+                Product currProd = tempList[i];
+                //Search for all temp tags in bridging table ProductTags, where the product ID = the current product ID
+                dynamic tempTag = (from pt in db.ProductTags
+                                where pt.ProductId.Equals(currProd.ProductId)
+                                select pt).DefaultIfEmpty();
+                //Create boolean keep, set to false
+                bool keep = false;
+
+                //For each Prodcut tag in list
+                foreach(Tag t in tempTag)
+                {
+                    //Loop through tag list passed, and check if they match
+                    foreach(int val in tags)
+                    {
+                        //If they match one of the tags
+                        if(val == t.TagID)
+                        {
+                            //set keep to true
+                            keep = true;
+                        }
+                    }
+                }
+                //If the product does not have the tag
+                if (!keep)
+                {
+                    //remove the product from the list
+                    tempList.RemoveAt(i);
+                }
+            }
+            //Return the list
+            return tempList;
             }
 
             public UserTable getAdmin(string uEmail)
@@ -635,6 +611,65 @@ namespace Team34_GP_IFM02B2_2023_WCF
             };
 
             return false;
+        }
+
+        /*public List<Product> getProductsFiltered(double Price1, double Price2, int SID, String cat) 
+        {
+            decimal temp1 = (decimal)Price1;
+            decimal temp2 = (decimal)Price2;
+            List<Product> returnList = new List<Product>();
+            dynamic prodList = (from p in db.Products
+                                where p.Price > temp1 && p.Price < temp2 && p.UserId == SID && p.ProductTags.Equals(cat)
+                                select p).DefaultIfEmpty();
+            if (prodList != null)
+            {
+                foreach (Product temp in prodList)
+                {
+                    Product newP = new Product
+                    {
+                        ProductId = temp.ProductId,
+                        Name = temp.Name,
+                        Picture = temp.Picture,
+                        Price = temp.Price,
+                    };
+                    returnList.Add(newP);
+
+                }
+                return returnList;
+            }
+            return null;
+        }*/
+
+        /*public List<Product> searchFiltered(double Price1, double Price2, int SID, String cat)
+        {
+            decimal temp1 = (decimal)Price1;
+            decimal temp2 = (decimal)Price2;
+            List<Product> returnList = new List<Product>();
+            dynamic prodList = (from p in db.Products
+                                where p.Price > temp1 && p.Price < temp2 && p.UserId == SID && p.ProductTags.con(cat)
+                                select p).DefaultIfEmpty();
+            if (prodList != null)
+            {
+                foreach (Product temp in prodList)
+                {
+                    Product newP = new Product
+                    {
+                        ProductId = temp.ProductId,
+                        Name = temp.Name,
+                        Picture = temp.Picture,
+                        Price = temp.Price,
+                    };
+                    returnList.Add(newP);
+
+                }
+                return returnList;
+            }
+            return null;
+        }*/
+
+        public string DeleteUser(string uEmail)
+        {
+            throw new NotImplementedException();
         }
     }
     }
