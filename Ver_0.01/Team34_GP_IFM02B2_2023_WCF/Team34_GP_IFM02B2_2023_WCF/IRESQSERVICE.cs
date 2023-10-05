2baa3dff1db0edf8e2b888cbbcdcb3696e74eeaa
Caitlyn Ross <caitlynross31@gmail.com>
2023-10-05 15:47:44 +0200

    Commiting recent changes to checkout page, commiting to update main to add a service function to get cart total

--- Ver_0.01/Team34_GP_IFM02B2_2023_WCF/Team34_GP_IFM02B2_2023_WCF/IRESQSERVICE.cs
+++ Ver_0.01/Team34_GP_IFM02B2_2023_WCF/Team34_GP_IFM02B2_2023_WCF/IRESQSERVICE.cs
@@ -54,6 +54,9 @@ namespace Team34_GP_IFM02B2_2023_WCF
         bool AddToCart(int pID, int uID, DateTime added, bool enabled);
 
         [OperationContract]
+        List<CartItem> GetCartTotal(int UID);
+
+        [OperationContract]
         List<CartItem> GetCart(int UID);
 
         [OperationContract]
--- Ver_0.01/Team34_GP_IFM02B2_2023_WebApp/Team34_GP_IFM02B2_2023_WebApp/checkout.aspx.cs
+++ Ver_0.01/Team34_GP_IFM02B2_2023_WebApp/Team34_GP_IFM02B2_2023_WebApp/checkout.aspx.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Team34_GP_IFM02B2_2023_WebApp.ResQReference;
 
 namespace Team34_GP_IFM02B2_2023_WebApp
 {
     public partial class checkout : System.Web.UI.Page
     {
+        ResQReference.RESQSERVICEClient sc = new ResQReference.RESQSERVICEClient();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UID"] == null)
+             {
+                int ID = Convert.ToInt32(Session["UID"]); //get user id
+              }
+
+            string products = "";
+            string totals = "";
+
+            List<CartItem> cart = (List<CartItem>)Session["CartList"];
+           
+            foreach(CartItem c in cart)
+            {
+                products += "<p>" + c.Product.Name+ "</p>";
+                products += "<p>R" + c.Product.Price + "</p> ";
+            }
+
+            order.InnerHtml = products;
+
+            totals += "<h6>"+ sc.getCat +"</h6>";
+            totals += "<h6>$150</h6>";
+           
+                            
 
         }
     }
 }
+
+            
+                           
