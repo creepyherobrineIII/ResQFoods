d6d808d68f956de263d4af6e392a9064d1492fd9
Cybermulan <77842538+Salma-Chan@users.noreply.github.com>
2023-10-07 15:57:05 +0200

    Update RegAdmin.aspx.cs

--- Ver_0.01/Team34_GP_IFM02B2_2023_WebApp/Team34_GP_IFM02B2_2023_WebApp/RegAdmin.aspx.cs
+++ Ver_0.01/Team34_GP_IFM02B2_2023_WebApp/Team34_GP_IFM02B2_2023_WebApp/RegAdmin.aspx.cs
@@ -1,1 +1,40 @@
+using HashPass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
+namespace Team34_GP_IFM02B2_2023_WebApp
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        ResQReference.RESQSERVICEClient sc = new ResQReference.RESQSERVICEClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            regStat.Visible = false;
+        }
+
+        protected void logReg_Click(object sender, EventArgs e)
+        {
+            regStat.Visible = false;
+            String email = uEmail.Value;
+            String pass = Secrecy.HashPassword(uPass.Value);
+            String cps = Secrecy.HashPassword(ucPass.Value);
+            if (pass.Equals(cps))
+            {
+                //use to lower and find the picture (some of the pictures arent jpg atm, need to fix that 
+                var regstr = sc.regAdmin(email, pass);
+                if (regstr)
+                {
+                    regStat.Visible = true;
+                    Server.Transfer("index.aspx");
+                }
+            }
+
+        }
+    }
+}
